fceb11bcf67c61e75a5d7b812949799590ebe185
Danilo Sanna <danilosanna@msn.com>
2021-02-24 16:35:59 -0300

    Lição 04 - CheckBox

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/gui/FXMLComboBox.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/gui/FXMLCheckBox.fxml"));
 			Scene scene = new Scene(root);
 			stage.setScene(scene);
 			stage.show();
---
+++ src/gui/FXMLCheckBoxControler.java
@@ -0,0 +1,36 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+
+public class FXMLCheckBoxControler implements Initializable{
+
+
+    @FXML
+    private CheckBox cbPhp;
+
+    @FXML
+    private CheckBox cbJava;
+
+    @FXML
+    private Button pegarValores;
+    
+    @FXML
+    private void handleCheckBoxButtonAction(ActionEvent e) {
+    	System.out.println("Valor de PHP: " + cbPhp.selectedProperty().getValue() );
+    	
+    	System.out.println("Valor de Java: " + cbJava.selectedProperty().getValue() );
+    }
+    
+    
+    
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+	}
+}
